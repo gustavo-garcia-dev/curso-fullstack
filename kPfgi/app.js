b24e3680d0bf25df0e45131925e548a036a989e5
gustavo-garcia-dev <gustavo.gribisilva@gmail.com>
2026-05-19 16:34:06 -0300

    feat: add arquivo

--- kPfgi/app.js
+++ kPfgi/app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+//middleware
+app.use(express.static('public'))
+
 //variavel com os dados 
 const alimentos = [
     {id: 1, nome: "feijão", preco: 10},
@@ -14,6 +17,7 @@ const tiposPratos = [
     {id: 1, nome: "strogonoff", preco: 40},
     {id: 2, nome: "lasanha", preco: 30},
     {id: 3, nome: "frango chadrez", preco: 25},
+    {id: 4, nome: "feijão com farinha", preco: 60}
 ]
 
 console.log(alimentos)
@@ -32,6 +36,12 @@ app.get('/produtos', (req, res) => {
 //tipos de pratos
 app.get('/cardapio', (req, res) => {
     res.send(tiposPratos)
+})
+
+//Rota home]
+app.get('/home', (req, res) => {
+    //console.log("ola", __dirname)
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 //executando servidor 
